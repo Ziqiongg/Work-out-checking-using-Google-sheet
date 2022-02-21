76c96f1b0349f023ea92c5a91e891e0287bb2ca3
Carol Li <ziqili33@gmail.com>
2022-02-21 17:16:46 +0000

    get apis and endpoint

---
+++ main.py
@@ -0,0 +1,13 @@
+API_ID = "fec0418d"
+API_KEY = "d1f0ccc987b32c3438cfa51daf80e53b"
+
+HEADERS = {
+    "x-app-id":API_ID,
+    "x-app-key":API_KEY
+}
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+parameters = {
+
+}
